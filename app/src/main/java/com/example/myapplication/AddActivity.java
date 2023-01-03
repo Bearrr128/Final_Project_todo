2a0b6eacd0d86cb77b78bd843ddee5a7350cde5b
yanyu0706 <r0920075266@gmail.com>
2023-01-03 23:55:56 +0800

    project

--- app/src/main/java/com/example/myapplication/AddActivity.java
+++ app/src/main/java/com/example/myapplication/AddActivity.java
@@ -1,15 +1,43 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
 
 public class AddActivity {
-    @Override
-    public void onClick(View view){
-        Intent intent = new Intent();
-        intent.setClass(MainActivity.this,AddActivity.class);
-        MainActivity.this.startActivity(intent);
+
+
+
+    public void onClick(View view) {
+
+        if (view == findViewById(R.id.finish)) {
+            NoteOperator noteOperator = new NoteOperator(AddActivity.this);
+            get_title = title.getText().toString().trim();
+            get_context = context.getText().toString().trim();
+
+            if (TextUtils.isEmpty(get_title) || TextUtils.isEmpty(get_context)) {
+                Toast.makeText(AddActivity.this, "添加信息不能为空", Toast.LENGTH_SHORT).show();
+            } else {
+                Note note = new Note();
+                note.title = get_title;
+                note.context = get_context;
+                boolean add = noteOperator.insert(note);
+                //如果添加数据成功，跳到待办事项界面，并通过传值，让目标界面进行刷新
+                if (add) {
+                    //Toast.makeText(AddActivity.this,"添加成功",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent();
+                    intent.setClass(AddActivity.this, MainActivity.class);
+                    intent.putExtra("Insert", 1);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(AddActivity.this, "添加失败", Toast.LENGTH_SHORT).show();
+                }
+            }
+        } else if (view == findViewById(R.id.back_add)) {
+            Intent intent = new Intent(AddActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+
     }
-
-
-
 }
--- app/src/main/java/com/example/myapplication/NoteOperator.java
+++ app/src/main/java/com/example/myapplication/NoteOperator.java
@@ -1,4 +1,87 @@
 package com.example.myapplication;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class NoteOperator {
-}
+    private DBHelper dbHelper;
+    public NoteOperator(Context context){
+
+        dbHelper=new DBHelper(context);
+    }
+
+    public boolean insert(Note note){
+
+        SQLiteDatabase db=dbHelper.getWritableDatabase();
+        ContentValues contentValues=new ContentValues();
+        contentValues.put(Note.KEY_title,note.title);
+        contentValues.put(Note.KEY_context,note.context);
+
+        long note_id=db.insert(Note.TABLE,null,contentValues);
+        db.close();
+        if(note_id!=-1)
+            return true;
+        else
+            return false;
+    }
+
+
+    public void delete(int note_id){
+
+        SQLiteDatabase db=dbHelper.getWritableDatabase();
+        db.delete(Note.TABLE,Note.KEY_id+"=?",new String[]{String.valueOf(note_id)});
+        db.close();
+    }
+
+
+    public ArrayList<HashMap<String,String>> getNoteList(){
+
+        SQLiteDatabase db=dbHelper.getReadableDatabase();
+        String sql="select "+Note.KEY_id+","+Note.KEY_title+","+Note.KEY_context+
+                " from "+Note.TABLE;
+
+        ArrayList<HashMap<String,String>> noteList=new ArrayList<HashMap<String,String>>();
+        Cursor cursor=db.rawQuery(sql,null);
+        while (cursor.moveToNext()){
+            HashMap<String,String> note=new HashMap<String,String>();
+            note.put("id",cursor.getString(cursor.getColumnIndex(Note.KEY_id)));
+            note.put("title",cursor.getString(cursor.getColumnIndex(Note.KEY_title)));
+            noteList.add(note);
+        }
+        cursor.close();
+        db.close();
+        return noteList;
+    }
+
+
+    public Note getNoteById(int id){
+
+        SQLiteDatabase db=dbHelper.getReadableDatabase();
+        String sql="select "+Note.KEY_title+","+Note.KEY_context+
+                " from "+Note.TABLE+" where "+Note.KEY_id+"=?";
+        Note note=new Note();
+        Cursor cursor=db.rawQuery(sql,new String[]{String.valueOf(id)});
+        while(cursor.moveToNext()) {
+            note.title = cursor.getString(cursor.getColumnIndex(Note.KEY_title));
+            note.context = cursor.getString(cursor.getColumnIndex(Note.KEY_context));
+        }
+        cursor.close();
+        db.close();
+        return note;
+    }
+
+    public void update(Note note){
+        SQLiteDatabase db=dbHelper.getWritableDatabase();
+        ContentValues contentValues=new ContentValues();
+
+        contentValues.put(Note.KEY_title,note.title);
+        contentValues.put(Note.KEY_context,note.context);
+
+        db.update(Note.TABLE,contentValues,Note.KEY_id+"=?",new String[]{String.valueOf(note.note_id)});
+        db.close();
+    }
