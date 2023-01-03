80b1b2af5407a76893950798d7c8e7588035efaf
xiu1021 <t109360151@ntut.org.tw>
2023-01-03 23:38:30 +0800

    edit

---
+++ app/src/main/java/com/example/myapplication/DetailActivity.java
@@ -0,0 +1,47 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+    Intent intent=getIntent();
+    note_id = intent.getIntExtra("note_id",0);
+    NoteOperator noteOperator=new NoteOperator( this);
+    ContactsContract.CommonDataKinds.Note note = noteOperator.getNoteById(note_id);
+
+        title.setText(String.valueOf(note.title));
+        context.setText(String.valueOf(note.context));
+        if(view == findViewById(R.id.save_detail)){
+            get_title=title.getText().toString().trim();
+            get_context=context.getText().toString().trim();
+        if(TextUtils.isEmpty(get_title)||TextUtils.isEmpty(get_context)){
+            Toast.makeText(this,"修改内容不能为空",Toast.LENGTH_SHORT).show();
+        }else{
+            ContactsContract.CommonDataKinds.Note note=new ContactsContract.CommonDataKinds.Note();
+            note.note_id=note_id;
+            note.title=get_title;
+            note.context=get_context;
+            NoteOperator noteOperator=new NoteOperator(DetailActivity.this);
+            noteOperator.update(note);
+
+            Toast.makeText(this,"修改成功",Toast.LENGTH_SHORT).show();
+            Intent intent=new Intent();
+            intent.setClass(DetailActivity.this,MainActivity.class);
+            startActivity(intent);
+
+        }
+    }
+
+}
